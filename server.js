36ee86547f2fa2458a9b99666b69bc82a72232a2
rasputtim <rasputtim@gmx.com>
2019-11-12 15:41:17 -0300

    adding graph views

--- server.js
+++ server.js
@@ -118,8 +118,7 @@ app.route('/login')
         //var params = {username: name , email: emailaddress, graph: graphHTML};
         //view
         //res.render('login',params);
-        res.render('login');
-        
+        res.render('login',{ user:  req.session.user, loggedin:false , index1_active:false, index2_active:false, index3_active:false ,index4_active:false,index4_active:false} );
     })
     .post((req, res) => {
         var username = req.body.username,
@@ -192,7 +191,7 @@ const balviewController = require('./controller').balview_controller;
 app.get('/inctrans', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
        
-        console.log("START TO FINDALL CATEGORIES");
+
     // Retrieve all Customer
     var cats;     ///This way is working ///////////////
     async.parallel( stackfunctions, function(err,result){
@@ -231,6 +230,94 @@ app.get('/inctrans', (req, res) => {
         res.redirect('/login');
     }
 });
+
+app.get('/spendbycat', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+       
+    // Retrieve all Customer
+    var cats;     ///This way is working ///////////////
+    async.parallel( stackfunctions, function(err,result){
+        var cats = result.categories;
+        var views = result.balviews;
+        //console.log("PARALLEL RESULTS FOR CATEGORIES: " + JSON.stringify(cats));
+        //console.log("\n==============================================\n");
+        //console.log("PARALLEL RESULTS FOR VIEWS: " + JSON.stringify(views));
+        res.render('spendbycat',{periods: views, cats:cats , user:  req.session.user, loggedin:true , index1_active:false, index2_active:false, index3_active:false ,index4_active:false,index4_active:false} );
+    
+    });
+    
+    ///todo: shall work in the module as well with callback. how?
+    /*Category.findAll().then(function (catgories) {
+        cats = JSON.stringify(catgories);
+        for (let i = 0; i < catgories.length; i++)  {
+            console.log(catgories[i].dataValues.name);
+          }
+        res.render('inctrans',{cats:catgories , user:  req.session.user, loggedin:true , index1_active:false, index2_active:false, index3_active:false ,index4_active:false,index4_active:false} );
+    });
+    */
+    
+    //console.log(req.cats);
+    //////////////  CALLBACK MODE WITH MODULE RETIEVEL////////////
+    //var categories;
+    //catlist.findAll(req,function(data, index) {
+    //    console.log(data);
+    //  });
+    //categories.forEach(function(data, index) {
+     //   console.log(data);
+     // });
+     ////////////////////////////////
+
+        
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+app.get('/cashflow', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+       
+    // Retrieve all Customer
+    var cats;     ///This way is working ///////////////
+    async.parallel( stackfunctions, function(err,result){
+        var cats = result.categories;
+        var views = result.balviews;
+        //console.log("PARALLEL RESULTS FOR CATEGORIES: " + JSON.stringify(cats));
+        //console.log("\n==============================================\n");
+        //console.log("PARALLEL RESULTS FOR VIEWS: " + JSON.stringify(views));
+        res.render('cashflow',{periods: views, cats:cats , user:  req.session.user, loggedin:true , index1_active:false, index2_active:false, index3_active:false ,index4_active:false,index4_active:false} );
+    
+    });
+    
+    ///todo: shall work in the module as well with callback. how?
+    /*Category.findAll().then(function (catgories) {
+        cats = JSON.stringify(catgories);
+        for (let i = 0; i < catgories.length; i++)  {
+            console.log(catgories[i].dataValues.name);
+          }
+        res.render('inctrans',{cats:catgories , user:  req.session.user, loggedin:true , index1_active:false, index2_active:false, index3_active:false ,index4_active:false,index4_active:false} );
+    });
+    */
+    
+    //console.log(req.cats);
+    //////////////  CALLBACK MODE WITH MODULE RETIEVEL////////////
+    //var categories;
+    //catlist.findAll(req,function(data, index) {
+    //    console.log(data);
+    //  });
+    //categories.forEach(function(data, index) {
+     //   console.log(data);
+     // });
+     ////////////////////////////////
+
+        
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+
 app.get('/form', (req, res) => {
     //if (req.session.user && req.cookies.user_sid) {
         res.render('form');
