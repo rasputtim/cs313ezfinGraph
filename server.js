6d861dce7527bb15e3f28ec4ca81b778cb984bd2
salvatore <salvatore@byui.edu>
2019-12-02 17:38:50 -0300

    fix routes to sessioncheck

--- server.js
+++ server.js
@@ -55,18 +55,14 @@ app.use(session({
 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-});
+var checkCookie = require('./routes/checkCookie');
 
 
 ////////////////////////////////
 // ROUTES //////////////////////
 ////////////////////////////////
 
+app.use(checkCookie);
 
 //NORMAL PAGES /////////////////
 require('./routes/logout.route')(app);
